b9d14d040af22461f75213237acd7b4f428aa70b
maksx312 <yamax0312@gmail.com>
2017-10-23 19:48:35 +0300

    Deleted some extra files

---
+++ SevenWonders.WebAPI/DTO/ReservationModel.cs
@@ -0,0 +1,29 @@
+﻿
+using SevenWonders.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SevenWonders.Models
+{
+    public class ReservationModel
+    {
+        public ReservationModel()
+        {
+            Rooms = new List<Room>();
+            TotalPrices = new List<decimal>();
+            PricesWithoutFood = new List<decimal>();
+            FoodInclude = new List<bool>();
+        }
+        public decimal MinPrice { get; set; }
+        public Reservation Reservation { get; set; }
+        public Hotel Hotel { get; set; }
+        public List<Room> Rooms { get; set; }
+        public List<decimal> TotalPrices { get; set; }
+        public List<decimal> PricesWithoutFood { get; set; }
+        public int Duration { get; set; }
+        public List<bool> FoodInclude { get; set; }
+        public bool Food { get; set; }
+    }
+}
