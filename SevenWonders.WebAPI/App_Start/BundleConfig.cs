94ba64d02cd90c3c7e89056be9169c9bb734e28d
Ulyana Kozub <ulyana.kozub@edvantis.com>
2017-12-02 23:49:53 +0200

    Changed directory for scripts and contents.

--- SevenWonders.WebAPI/App_Start/BundleConfig.cs
+++ SevenWonders.WebAPI/App_Start/BundleConfig.cs
@@ -9,35 +9,35 @@ namespace SevenWonders.WebAPI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/static/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            "~/Scripts/jquery-ui-{version}.js"));
+            "~/static/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/static/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/static/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/select2.js"
+                      "~/static/Scripts/bootstrap.js",
+                      "~/static/Scripts/respond.js",
+                      "~/static/Scripts/select2.js"
                       ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/vendor/font-awesome/css/font-awesome.min.css",
-                      "~/Content/vendor/magnific-popup/magnific-popup.css",
-                      "~/Content/creative.css"));
+                      "~/static/Content/bootstrap.css",
+                      "~/static/Content/site.css",
+                      "~/static/Content/vendor/font-awesome/css/font-awesome.min.css",
+                      "~/static/Content/vendor/magnific-popup/magnific-popup.css",
+                      "~/static/Content/creative.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/Statistic").Include(
-                "~/scripts/Statistic.js",
-                "~/Scripts/Table.js"));
+                "~/static/scripts/Statistic.js",
+                "~/static/Scripts/Table.js"));
         }
     }
 }
